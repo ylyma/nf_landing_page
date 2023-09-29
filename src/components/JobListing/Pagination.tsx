b9e2d52b23381ad24f631862e91c7f3bd242cded
ylyma <amylingyuhui@gmail.com>
2023-09-29 13:42:48 +0800

    Finish pagination

--- src/components/JobListing/Pagination.tsx
+++ src/components/JobListing/Pagination.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
 import "./Pagination.css";
 
 type Props = {
@@ -15,35 +16,82 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }: Props) => {
   const prevPage = () => {
     if (currentPage !== 1) setCurrentPage(currentPage - 1);
   };
+
   return (
-    <nav>
-      <ul className="pagination justify-content-center">
-        <li className="page-item">
-          <a className="page-link" onClick={prevPage} href="#">
-            ‹
-          </a>
-        </li>
-        {pageNumbers.map((pgNumber) => (
-          <li
-            key={pgNumber}
-            className={`page-item ${currentPage == pgNumber ? "active" : ""} `}
-          >
-            <a
-              onClick={() => setCurrentPage(pgNumber)}
-              className="page-link"
-              href="#"
-            >
-              {pgNumber}
-            </a>
-          </li>
-        ))}
-        <li className="page-item">
-          <a className="page-link" onClick={nextPage} href="#">
-            »
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="pagination__container">
+      <nav>
+        <div className="pagination__text">
+          <span>
+            Page <b>{currentPage}</b> of <b>{nPages}</b>
+          </span>
+        </div>
+
+        <ul className="pagination__pages">
+          {currentPage === 1 ? (
+            <></>
+          ) : (
+            <>
+              <li>
+                <a
+                  className="pagination__page-link"
+                  onClick={nextPage}
+                  href="#"
+                >
+                  «
+                </a>
+              </li>
+              <li>
+                <a
+                  className="pagination__page-link"
+                  onClick={prevPage}
+                  href="#"
+                >
+                  ⟨
+                </a>
+              </li>
+            </>
+          )}
+
+          {pageNumbers.map((pgNumber) => (
+            <li key={pgNumber}>
+              <a
+                onClick={() => setCurrentPage(pgNumber)}
+                className={`pagination__page-link ${
+                  currentPage === pgNumber ? "active" : ""
+                }`}
+                href="#"
+              >
+                {pgNumber}
+              </a>
+            </li>
+          ))}
+          {currentPage === nPages ? (
+            <></>
+          ) : (
+            <>
+              <li>
+                <a
+                  className="pagination__page-link"
+                  onClick={nextPage}
+                  href="#"
+                >
+                  ⟩
+                </a>
+              </li>
+              <li>
+                <a
+                  className="pagination__page-link"
+                  onClick={prevPage}
+                  href="#"
+                >
+                  »
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
